99b7b1304a20b3efcf5f5fb16c24131e8c53c274
Erwan Lacoste <erwan.lacoste+git@gmail.com>
2019-11-11 17:33:16 +0100

    distinguish v23 from v27 and keep in memory

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const noble = require('noble-mac');
-const scanningTimeout = 15000; // one second
+const scanningTimeout = 15000;
 const scanningRepeat = scanningTimeout + 5000; // Repeat scanning after 10 seconds for new peripherals.
 
 const mqtt = require('mqtt');
 const client = mqtt.connect('mqtt://centralpi');
-
-const devices = [
-  'terasse',
-  'pupuce'/*,
-  'studio',
-  'salon'*/
-];
 
 noble.on('stateChange', function(state) {
   console.log('state changed!! ' + state);
@@ -43,21 +36,16 @@ noble.on('stateChange', function(state) {
 noble.on('discover', function(peripheral) {
 
   const advertisement = peripheral.advertisement;
-  const localName = advertisement.localName;
-  // var txPowerLevel = advertisement.txPowerLevel;
   const manufacturerData = advertisement.manufacturerData;
-  // var serviceData = advertisement.serviceData;
-  // var serviceUuids = advertisement.serviceUuids;
   if (isDeviceCompatible(advertisement)) {
-    console.log('Found peripheral:', localName);
-    console.log(peripheral);
-    console.log(advertisement);
-    // // console.log(peripheral.id + ', ' + serviceData + ', ' + serviceUuids + ', ' + txPowerLevel);
-    // console.log(peripheral.id + ', ' + peripheral.address);
-    // //console.log(peripheral);
-    // console.log(manufacturerData);
-    console.log(manufacturerData.toString('hex'));
-    decodeTempo(manufacturerData, localName);
+    //console.log('Found peripheral:', advertisement.localName);
+    //console.log(manufacturerData.toString('hex'));
+    const desc = fetchDeviceDescription(peripheral);
+    if (desc) {
+      const data = decodeTempo(manufacturerData, desc);
+      client.publish('sensor', JSON.stringify(data),
+        function(err) {if (err) console.log(err);});
+    }
   }
 }); // End on Noble Discover!
 
@@ -74,37 +62,86 @@ function isDeviceCompatible(advertisement) {
   return false;
 }
 
-function decodeTempo(buf, localName) {
+// keeps track of discovered devices
+const deviceMap = {};
+
+function fetchDeviceDescription(peripheral) {
+  const uuid = peripheral.uuid;
+  if (deviceMap[uuid] === undefined) {
+    deviceMap[uuid] = describeDevice(peripheral);
+  }
+  return deviceMap[uuid];
+}
+
+function describeDevice(peripheral) {
+  const adv = peripheral.advertisement;
+  if (adv.manufacturerData.length <= 16) {
+    return {
+      version: adv.manufacturerData.readUInt8(2),
+      label: adv.localName
+    };
+  }
+  return undefined;
+}
+
+function decodeTempo(buf, description) {
   const data = {};
   //console.log('buffer length:', buf.length);
   if (buf.length <= 16) {
-    data.version = buf.readUInt8(2);
-    data.battery = buf.readUInt8(3);
-    data.timeInterval = buf.readUInt16BE(4);
-    data.timeIntervalPosition = buf.readUInt16BE(6);
-    data.temperature = buf.readInt16BE(8) / 10;
-    data.humidity = buf.readInt16BE(10) / 10;
-    data.dewPoint = buf.readInt16BE(12) / 10;
+    decodeShortBeacon(buf, description, data);
   } else {
-    data.highestTemperature = buf.readInt16BE(2) / 10;
-    data.highestHumidity = buf.readInt16BE(4) / 10;
-    data.lowestTemperature = buf.readInt16BE(6) / 10;
-    data.lowestHumidity = buf.readInt16BE(8) / 10;
-    data.highest24Temperature = buf.readInt16BE(10) / 10;
-    data.highest24Humidity = buf.readInt16BE(12) / 10;
-    data.lowest24Temperature = buf.readInt16BE(14) / 10;
-    data.lowest24Humidity = buf.readInt16BE(16) / 10;
-    data.average24Temperature = buf.readInt16BE(18) / 10;
-    data.average24humidity = buf.readInt16BE(20) / 10;
-    data.data1 = buf.readInt16BE(22) / 10;
-    data.data2 = buf.readInt16BE(24) / 10;
+    decodeLongBeacon(buf, data, description);
   }
-  const wrapper = {
+  return {
     timestamp: new Date(),
-    name: localName,
+    name: description.label,
+    version: description.version,
     data: data
   };
-  client.publish('sensor', JSON.stringify(wrapper));
-  console.log(data);
 }
 
+function decodeShortBeacon(buf, description, data) {
+  data.version = buf.readUInt8(2);
+  data.battery = buf.readUInt8(3);
+  data.timeInterval = buf.readUInt16BE(4);
+  data.timeIntervalPosition = buf.readUInt16BE(6);
+  data.temperature = buf.readInt16BE(8) / 10;
+  data.humidity = buf.readInt16BE(10) / 10;
+  data.mode = buf.readInt8(14);
+  data.breachCount = buf.readUInt8(15);
+  if (description.version === 23) {
+    data.dewPoint = buf.readInt16BE(12) / 10;
+  } else if (description.version === 37) {
+    data.pressure = buf.readInt16BE(12) / 10;
+  }
+}
+
+function decodeLongBeacon(buf, data, description) {
+  data.highest24Temperature = buf.readInt16BE(10) / 10;
+  data.highest24Humidity = buf.readInt16BE(12) / 10;
+  data.lowest24Temperature = buf.readInt16BE(14) / 10;
+  data.lowest24Humidity = buf.readInt16BE(16) / 10;
+  data.average24Temperature = buf.readInt16BE(18) / 10;
+  data.average24humidity = buf.readInt16BE(20) / 10;
+  data.globalIdentifier = buf.readUInt8(22);
+  data.referenceData = buf.readUInt32BE(23);
+  if (description.version === 23) {
+    decodeLongTempoBeacon(buf, data);
+  } else if (description.version === 27) {
+    decodeLongPebbleBeacon(buf, data);
+  }
+}
+
+function decodeLongTempoBeacon(buf, data) {
+  data.highestTemperature = buf.readInt16BE(2) / 10;
+  data.highestHumidity = buf.readInt16BE(4) / 10;
+  data.lowestTemperature = buf.readInt16BE(6) / 10;
+  data.lowestHumidity = buf.readInt16BE(8) / 10;
+}
+
+function decodeLongPebbleBeacon(buf, data) {
+  data.highest24Pressure = buf.readInt16BE(2) / 10;
+  data.average24Pressure = buf.readInt16BE(4) / 10;
+  data.lowest24Pressure = buf.readInt16BE(6) / 10;
+  data.altitude = buf.readInt16BE(8) / 10;
+}
